fa5ff758d400369d7040602f35ef6c4e2a024efe
Gryzz1y <arbuz111222333@gmail.com>
2026-02-11 23:01:57 +0000

    First Steps with Tutorial to setup Gradle Project

---
+++ GameEngine/src/main/java/com/Gryzz1y/Engine.java
@@ -0,0 +1,7 @@
+package com.Gryzz1y;
+
+public class Engine{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ GameEngine/src/test/java/EngineTest.java
@@ -0,0 +1,11 @@
+package com.Gryzz1y;
+
+import org.junit.Test;
+
+public class EngineTest {
+    
+    @Test
+    public void verifyNoExceptationThrown() {
+        Engine.main(new String[]{});
+    }
+}
